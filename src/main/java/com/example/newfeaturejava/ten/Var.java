b9196fa7ffc77e0be789fc74ffaec0d8b2daf295
d-dxz <1205760997@qq.com>
2022-12-15 16:59:55 +0800

    java新特性

---
+++ src/main/java/com/example/newfeaturejava/ten/Var.java
@@ -0,0 +1,13 @@
+package com.example.newfeaturejava.ten;
+
+/**
+ * @Author: diaoxiuze
+ * @Date: 2022/12/15 16:55
+ */
+public class Var {
+    public static void main(String[] args) {
+        var name = "Mohamed Taman";
+        var lastName = name.substring(8);
+        System.out.println("Value: "+lastName +" ,and type is: "+ lastName);
+    }
+}
---
+++ src/test/java/com/example/newfeaturejava/NewFeatureJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.newfeaturejava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewFeatureJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
